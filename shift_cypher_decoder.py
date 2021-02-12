424ce408284fd05dc50ee5bf289e0ea28ef42f22
gkschnettgoecke <grant.schnettgoecke@gmail.com>
2021-02-12 17:20:34 -0600

    Create shift_cypher_decoder.py

---
+++ shift_cypher_decoder.py
@@ -0,0 +1,15 @@
+from collections import deque
+key = deque(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+lang_map = { 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z':25}
+
+print('Enter cypher text:')
+cypher_text = input()
+
+for k in range(1, 26):
+    decoded_text = ""
+    key.rotate(1)
+    for l in cypher_text:
+        if l != " ":
+            decoded_text = decoded_text + key[lang_map[l]] 
+
+    print(f"+{k}: {decoded_text}")
